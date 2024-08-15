5a48970c10640cdbc2881be591e47ab77b8427a4
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-15 19:25:52 +0300

    mobx basic usage

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:fluttermvvmtemplate/core/base/state/base_state.dart';
 import 'package:fluttermvvmtemplate/core/base/view/base_view.dart';
+import 'package:fluttermvvmtemplate/view/authenticate/test/viewmodel/test_view_model.dart';
 
 class TestView extends StatefulWidget {
   const TestView({super.key});
@@ -10,11 +12,36 @@ class TestView extends StatefulWidget {
 }
 
 class _TestViewState extends BaseState<TestView> {
+  late TestViewModel viewModel;
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-      viewModel: "test",
-      onPageBuilder: (context, value) => Text(value),
+    return BaseView<TestViewModel>(
+      viewModel: viewModel,
+      onModelReady: (model) {
+        viewModel = model;
+      },
+      onPageBuilder: (context, value) => getScaffoldBody,
     );
+  }
+
+  Widget get getScaffoldBody => Scaffold(
+        floatingActionButton: floatingActionButtonIncrement,
+        body: buildTextNumber,
+      );
+
+  FloatingActionButton get floatingActionButtonIncrement {
+    return FloatingActionButton(
+      onPressed: () {
+        viewModel.incrementNumber();
+      },
+    );
+  }
+
+  Widget get buildTextNumber {
+    return Observer(builder: (context) {
+      return Text(
+        viewModel.number.toString(),
+      );
+    });
   }
 }
