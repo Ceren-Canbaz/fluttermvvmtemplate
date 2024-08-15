e1cabf863a5ae24459e073f367668cd321ccd63f
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-15 23:22:23 +0300

    text theme extension added

--- lib/core/extension/context_extension.dart
+++ lib/core/extension/context_extension.dart
@@ -11,4 +11,5 @@ extension MediaQueryExtension on BuildContext {
 
 extension ThemeExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => theme.textTheme;
 }
