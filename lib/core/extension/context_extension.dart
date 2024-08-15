32ea798a3ab8c2df317d0a53b1995e799a8a356c
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-15 23:27:15 +0300

    padding extension added

--- lib/core/extension/context_extension.dart
+++ lib/core/extension/context_extension.dart
@@ -7,9 +7,36 @@ extension ContextExtension on BuildContext {
 extension MediaQueryExtension on BuildContext {
   double get height => mediaQuery.size.height;
   double get width => mediaQuery.size.width;
+  double get lowValue => height * 0.01;
+  double get normalValue => height * 0.02;
+  double get mediumValue => height * 0.04;
+  double get highValue => height * 0.1;
 }
 
 extension ThemeExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => theme.textTheme;
+  ColorScheme get colors => theme.colorScheme;
+}
+
+extension PaddingExtension on BuildContext {
+  EdgeInsets get allPaddingLow => EdgeInsets.all(lowValue);
+  EdgeInsets get allPpaddingNormal => EdgeInsets.all(normalValue);
+  EdgeInsets get allPpaddingMedium => EdgeInsets.all(mediumValue);
+  EdgeInsets get allPpaddingHigh => EdgeInsets.all(highValue);
+  EdgeInsets get horizontalPaddingLow =>
+      EdgeInsets.symmetric(horizontal: lowValue);
+  EdgeInsets get horizontalPaddingNormal =>
+      EdgeInsets.symmetric(horizontal: normalValue);
+  EdgeInsets get horizontalPaddingMedium =>
+      EdgeInsets.symmetric(horizontal: mediumValue);
+  EdgeInsets get horizontalPaddingHigh =>
+      EdgeInsets.symmetric(horizontal: highValue);
+  EdgeInsets get verticalPaddingLow => EdgeInsets.symmetric(vertical: lowValue);
+  EdgeInsets get verticalPaddingNormal =>
+      EdgeInsets.symmetric(vertical: normalValue);
+  EdgeInsets get verticalPaddingMedium =>
+      EdgeInsets.symmetric(vertical: mediumValue);
+  EdgeInsets get verticalPaddingHigh =>
+      EdgeInsets.symmetric(vertical: highValue);
 }
