01fc7d58f74cbe99ca85f44bbf7933e330ac472f
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-15 21:19:52 +0300

    navigation and theme provider added to app provider

--- lib/core/init/notifier/provider_list.dart
+++ lib/core/init/notifier/provider_list.dart
@@ -1,3 +1,6 @@
+import 'package:fluttermvvmtemplate/core/init/navigation/navigation_service.dart';
+import 'package:fluttermvvmtemplate/core/init/notifier/theme_notifier.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 class ApplicationProvider {
@@ -9,6 +12,11 @@ class ApplicationProvider {
 
   ApplicationProvider._init();
   List<SingleChildWidget> singleItem = [];
-  List<SingleChildWidget> dependItems = [];
+  List<SingleChildWidget> dependItems = [
+    ChangeNotifierProvider(
+      create: (context) => ThemeNotifier(),
+    ),
+    Provider.value(value: NavigationService),
+  ];
   List<SingleChildWidget> uiChangerItems = [];
 }
