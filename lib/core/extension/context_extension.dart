32ac168f7d90ef698f06ff0c60a881a5aba2ab99
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-15 23:21:48 +0300

    theme and size extensions added

--- lib/core/extension/context_extension.dart
+++ lib/core/extension/context_extension.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 extension ContextExtension on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+}
+
+extension MediaQueryExtension on BuildContext {
+  double get height => mediaQuery.size.height;
+  double get width => mediaQuery.size.width;
+}
+
+extension ThemeExtension on BuildContext {
+  ThemeData get theme => Theme.of(this);
 }
