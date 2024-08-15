cd8f0b053cecfa2009f2b73f462b3888636e2e2f
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-15 20:07:54 +0300

    easy localization usage

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,7 +1,12 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:fluttermvvmtemplate/core/base/state/base_state.dart';
 import 'package:fluttermvvmtemplate/core/base/view/base_view.dart';
+import 'package:fluttermvvmtemplate/core/components/text/locale_text.dart';
+import 'package:fluttermvvmtemplate/core/extension/string_extension.dart';
+import 'package:fluttermvvmtemplate/core/init/lang/locale_keys.g.dart';
 import 'package:fluttermvvmtemplate/view/authenticate/test/viewmodel/test_view_model.dart';
 
 class TestView extends StatefulWidget {
@@ -25,9 +30,25 @@ class _TestViewState extends BaseState<TestView> {
   }
 
   Widget get getScaffoldBody => Scaffold(
+        appBar: AppBar(
+          actions: [changeLanguageIcon],
+          title: welcomeText,
+        ),
         floatingActionButton: floatingActionButtonIncrement,
         body: buildTextNumber,
       );
+
+  LocaleText get welcomeText {
+    return const LocaleText(
+      value: LocaleKeys.welcome,
+    );
+  }
+
+  IconButton get changeLanguageIcon => IconButton(
+      onPressed: () {
+        context.setLocale(const Locale("tr", "TR"));
+      },
+      icon: const Icon(Icons.language));
 
   FloatingActionButton get floatingActionButtonIncrement {
     return FloatingActionButton(
